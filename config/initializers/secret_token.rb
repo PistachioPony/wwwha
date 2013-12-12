7995ef6521b4ca22e8efd1e483958f8650b12912
Maria Mercedes Martinez <pistachiopony@gmail.com>
2013-12-12 16:11:21 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wwwha::Application.config.secret_key_base = 'eaa5ffa5b341f7c739c76fec6cc3d09a35edfcebfe99db4746ac7b7d5bd758c63ed9740e23e5665a431041cc6b138318ba5d6188d95f53fffc92238ca5f6ef60'
