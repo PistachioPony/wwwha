c31d307e6ea0c180bf39a4664fc3eb5e0316e01a
Maria Mercedes Martinez <pistachiopony@gmail.com>
2013-12-15 15:38:05 -0500

    begin building player controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,4 +3,14 @@ class PlayersController < ApplicationController
   def index
     @player = Player.all
   end
+
+  # GET /users/1
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  # GET /users/new
+  def new
+    @player = Player.new
+  end
 end
